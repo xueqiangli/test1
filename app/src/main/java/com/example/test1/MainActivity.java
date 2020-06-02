84165a677684fc8a5538b7518a8ad876586b7ff2
xueqiangli <275541894@qq.com>
2020-06-02 13:04:21 +0800

    给demo写实例

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -4,18 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mylibrary.Plus;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnComulate;
+    EditText edtA,edtB;
+    TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        int c=Plus.sum(1,2);
-        Log.e("BBB","c"+c);
+        btnComulate=findViewById(R.id.btn_comulate);
+        edtA=findViewById(R.id.edt_a);
+        edtB=findViewById(R.id.edt_b);
+        txtResult=findViewById(R.id.txt_result);
+        btnComulate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtResult.setText(Plus.sum(Integer.parseInt(edtA.getText().toString()),Integer.parseInt(edtB.getText().toString()))+"");
+            }
+        });
 
     }
 }
